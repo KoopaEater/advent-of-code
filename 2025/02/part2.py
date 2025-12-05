fe674d13cf939d3541108cd242d2bc3e33e556e8
Max Kørner Andersen <maxkandersen@gmail.com>
2025-12-05 23:51:48 +0100

    02-Finish: Gold star

--- 2025/02/part2.py
+++ 2025/02/part2.py
@@ -1,5 +1,5 @@
 
-with open("input_example.txt", "r") as input:
+with open("input.txt", "r") as input:
     line = input.readline()
     search_groups = line.split(',')
     sum = 0
@@ -10,7 +10,7 @@ with open("input_example.txt", "r") as input:
         for i in range(low, high + 1):
             str_i = str(i)
             len_i = len(str_i)
-            for repeat_len in range(1, len_i // 2):
+            for repeat_len in range(1, len_i // 2 + 1):
                 if len_i % repeat_len != 0:
                     continue
                 repeat_str = str_i[:repeat_len]
