5a4d2d284a23b6bf26459c4479da24ca7e5f3574
Max Kørner Andersen <maxkandersen@gmail.com>
2025-12-09 22:32:19 +0100

    04-Finish: Silver star

--- 2025/04/part1.py
+++ 2025/04/part1.py
@@ -1,5 +1,5 @@
 
-with open("input_example.txt") as input:
+with open("input.txt") as input:
     lines = input.readlines()
     line_len = len(lines[0].strip())
     boogie = []
@@ -9,5 +9,19 @@ with open("input_example.txt") as input:
         new_line = "0" + line + "0"
         boogie.append(new_line)
     boogie.append("0"*(line_len+2))
-    for line in boogie:
-        print(line)
+    forklift = 0
+    for y in range(1, len(lines) + 1):
+        for x in range(1, line_len + 1):
+            if boogie[y][x] == "0":
+                continue
+            rolls = 0
+            search = [-1, 0, 1]
+            for xoff in search:
+                for yoff in search:
+                    if xoff == 0 and yoff == 0:
+                        continue
+                    spot = int(boogie[y + yoff][x + xoff])
+                    rolls += spot
+            if rolls < 4:
+                forklift += 1
+    print(forklift)
