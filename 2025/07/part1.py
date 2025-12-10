8cabfaabe4a0b15b668b6263bc25e906580f02b1
Max Kørner Andersen <maxkandersen@gmail.com>
2025-12-10 23:46:34 +0100

    07-Finish: Silver star

---
+++ 2025/07/part1.py
@@ -0,0 +1,25 @@
+
+lines = []
+poses = set()
+
+with open("input.txt") as input:
+    all_lines = [line.strip() for line in input.readlines()]
+    lines.extend(all_lines[1:])
+    start_pos = all_lines[0].find("S")
+    poses.add(start_pos)
+
+splits = 0
+for i, line in enumerate(lines):
+    new_poses = set()
+    for pos in poses:
+        if line[pos] == "^":
+            splits += 1
+            if pos > 0:
+                new_poses.add(pos-1)
+            if pos < len(line) - 1:
+                new_poses.add(pos + 1)
+        else:
+            new_poses.add(pos)
+    poses = new_poses
+
+print(splits)
